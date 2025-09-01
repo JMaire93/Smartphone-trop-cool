0bd3202274f5154292386789a4bae52cdf525f9c
Nihel-DWWM <nihel.boussetta.dev@gmail.com>
2025-09-01 15:29:03 +0200

    Fonctionnalité Contact

--- js/contact.js
+++ js/contact.js
@@ -1,1 +1,19 @@
 console.log("contact")
+document.addEventListener("DOMContentLoaded", () => {
+  const form = document.getElementById("contactForm");
+  const modal = new bootstrap.Modal(document.getElementById("confirmationModal"));
+
+  form.addEventListener("submit", function (event) {
+    event.preventDefault();
+    event.stopPropagation();
+
+    if (!form.checkValidity()) {
+      form.classList.add("was-validated");
+    } else {
+      // Si tout est valide → afficher modale
+      modal.show();
+      form.reset();
+      form.classList.remove("was-validated");
+    }
+  });
+});
